da671eac9efb093071648f503b1f6571506b69dd
Rajasurya <rajasurya2006rjs@gmail.com>
2025-10-24 21:13:51 +0530

    Prepare for Render deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,12 +22,13 @@ app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
+
 
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
